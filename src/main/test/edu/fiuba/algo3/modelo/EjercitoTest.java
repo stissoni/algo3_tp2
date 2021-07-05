a307f9ff0941950d37b6c422ce25f87a41ed5087
Santiago Tissoni <santi.tissoni@gmail.com>
2021-07-04 21:58:21 -0300

    Se corrigen nombres, y se elimina clase de ejemplo.

--- src/main/test/edu/fiuba/algo3/modelo/EjercitoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/EjercitoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class EjecitoTest {
+public class EjercitoTest {
     @Test
     public void testNumeroDeTropasDeEjercito(){
         Ejercito ejercito = new Ejercito(3);

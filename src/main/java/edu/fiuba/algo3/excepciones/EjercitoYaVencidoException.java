859ecbb601f66318aa82e876bdcc297d06f5b394
lambda~ <montero987@gmail.com>
2021-07-11 08:17:04 -0300

    [Clase Partida]: Eliminé Código repetido con el uso de un wrapper.

--- src/main/java/edu/fiuba/algo3/excepciones/EjercitoYaVencidoException.java
+++ src/main/java/edu/fiuba/algo3/excepciones/EjercitoYaVencidoException.java
@@ -1,9 +1,6 @@
 package edu.fiuba.algo3.excepciones;
 
 public class EjercitoYaVencidoException extends Throwable {
-    public EjercitoYaVencidoException(String mensajeError){
-        super(mensajeError);
-    }
     public EjercitoYaVencidoException(String mensajeError, Throwable causa){
         super(mensajeError, causa);
     }
